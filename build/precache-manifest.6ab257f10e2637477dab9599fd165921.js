dac6f569bf44cbb84b8d101be2139d4642672ca0
Olabiran Joshua <olabiranj@gmail.com>
2020-03-12 09:37:44 +0100

    pwa

---
+++ build/precache-manifest.6ab257f10e2637477dab9599fd165921.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d630c7be03428c5346f4",
+    "url": "/static/js/main.d630c7be.chunk.js"
+  },
+  {
+    "revision": "4e38724f1e55a54fe097",
+    "url": "/static/js/1.4e38724f.chunk.js"
+  },
+  {
+    "revision": "6094f837e401065d3cbf79fcfb0b1e81",
+    "url": "/index.html"
+  }
+];
